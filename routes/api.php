34bf22445b241859e43f5e1d60290f41a9a2cf06
Yernar Abilbay <eabilbay@gmail.com>
2021-03-07 19:54:09 +0600

    removed unused code

--- routes/api.php
+++ routes/api.php
@@ -6,18 +6,14 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/unauthorized', function () {
-   return response()->json(['error' => 'Unauthorized']);
+    return response()->json(['error' => 'Unauthorized']);
 })->name('unauthorized');
 
 Route::group([
     'prefix' => 'auth'
 ], function () {
-
     Route::post('login', [AuthController::class, 'login']);
-
-    Route::group(['middleware' => 'is_auth'], function () {
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
-        Route::post('me', [AuthController::class, 'me']);
-    });
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
